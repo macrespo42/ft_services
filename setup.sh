4ec8b16258f0150924f564710e1740d4327fa5cf
macrespo42 <maxime.crespopro@gmail.com>
2020-09-03 15:06:20 +0200

    make setup script easily scalable

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ minikube_start() {
     minikube start  --vm-driver=docker \
                     --cpus=2
     minikube addons enable metrics-server
-    minikube addons enable dashboard
+    minikube addons enable dashboard >> /dev/null
     minikube addons enable metallb
     eval $(minikube docker-env)
     MINIKUBE_IP="$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)"
@@ -18,11 +18,24 @@ config_load_balancer() {
     sed -i "s/$MINIKUBE_IP/#MINIKUBE_IP/g" ./srcs/metallb_config.yaml
 }
 
+create_k8s_object() {
+    files=("deployment" "service")
+    path_yaml="./srcs/$1/yaml"
+    for item in ${files[*]}; do
+        if [ -e ${path_yaml}/${item}.yaml ]; then
+            if [ $2 = "create" ]; then
+                kubectl create -f ${path_yaml}/${item}.yaml
+            else
+                kubectl delete -f ${path_yaml}/${item}.yaml
+            fi
+        fi
+    done
+}
+
 main() {
     minikube_start
-    kubectl create -f ./srcs/deployment.yaml
     config_load_balancer
-    kubectl create -f ./srcs/services.yaml
+    create_k8s_object "nginx" "create"
     echo "IP of minikube is : $MINIKUBE_IP"
 }
 
