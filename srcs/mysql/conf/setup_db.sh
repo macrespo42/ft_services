6461d8dfdfa901ebf06217084b924836b3e0a8f7
macrespo42 <maxime.crespopro@gmail.com>
2020-09-21 14:38:05 +0200

    mysql launch but db empty

--- srcs/mysql/conf/setup_db.sh
+++ srcs/mysql/conf/setup_db.sh
@@ -12,9 +12,9 @@ if [ ! -d /var/lib/mysql/mysql ]; then
     chown -R mysql /var/lib/mysql
 fi
 
-echo "CREATE DATABASE wordpress;" | mysql -u root 
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY 'wordpress password';" | mysql -u root 
-echo "FLUSH PRIVILEGES;" | mysql -u root 
-echo "EXIT;" | mysql -u root 
+echo "CREATE DATABASE wordpress;" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY 'wordpress password';" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
+echo "EXIT;" | mysql -u root
 
 exec /usr/bin/mysqld --user=mysql --console
