a06f50f64e99c99a7a9c7b94dbd9b9e719352ab0
macrespo42 <maxime.crespopro@gmail.com>
2020-10-01 13:36:06 +0200

    change config in influx

--- srcs/influxdb/conf/start.sh
+++ srcs/influxdb/conf/start.sh
@@ -10,6 +10,9 @@ fi
 
 influxd &
 sleep 5
-curl -XPOST "http://localhost:8086/query" --data-urlencode "q=CREATE USER $INFLUX_USER WITH PASSWORD '$INFLUX_PASSWORD' WITH ALL PRIVILEGES"
+# curl -XPOST "http://localhost:8086/query" --data-urlencode "q=CREATE USER $INFLUX_USER WITH PASSWORD '$INFLUX_PASSWORD' WITH ALL PRIVILEGES"
+influx -execute "CREATE DATABASE influx_db"
+influx -execute "CREATE USER $INFLUX_USER WITH PASSWORD '$INFLUX_PASSWORD'"
+influx -execute "GRANT ALL ON influx_db TO $INFLUX_USER"
 pkill influxd
 influxd
