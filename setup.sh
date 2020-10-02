532ebc6c205fa523fc99d15a68e506a8dcb07529
macrespo42 <maxime.crespopro@gmail.com>
2020-10-02 16:40:49 +0200

    fix dashboard

--- setup.sh
+++ setup.sh
@@ -5,24 +5,24 @@ minikube_init() {
     minikube start  --driver=docker \
                     --cpus=2
     minikube addons enable metrics-server
-    minikube addons enable dashboard >> /dev/null
-    kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.1/manifests/metallb.yaml
+    minikube addons enable dashboard
     eval $(minikube docker-env)
 }
 
 config_load_balancer() {
     metallb_config_file="./srcs/metallb_config.yaml"
+    kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.1/manifests/metallb.yaml
     kubectl apply -f $metallb_config_file
 }
 
 build_images() {
     docker build -t nginx_alpine ./srcs/nginx/ >> /dev/null
-    # docker build -t ftps_alpine ./srcs/ftps/ >> /dev/null
-    # docker build -t wordpress_alpine ./srcs/wordpress/ >> /dev/null
-    # docker build -t mysql_alpine ./srcs/mysql/ >> /dev/null
-    # docker build -t php_my_admin_alpine ./srcs/phpMyAdmin/ >> /dev/null
-    docker build -t grafana_alpine ./srcs/grafana/
-    docker build -t influxdb_alpine ./srcs/influxdb/
+    docker build -t ftps_alpine ./srcs/ftps/ >> /dev/null
+    docker build -t wordpress_alpine ./srcs/wordpress/ >> /dev/null
+    docker build -t mysql_alpine ./srcs/mysql/ >> /dev/null
+    docker build -t php_my_admin_alpine ./srcs/phpMyAdmin/ >> /dev/null
+    docker build -t grafana_alpine ./srcs/grafana/ >> /dev/null
+    docker build -t influxdb_alpine ./srcs/influxdb/ >> /dev/null
 }
 
 toogle_k8s_object() {
@@ -41,13 +41,13 @@ toogle_k8s_object() {
 
 main() {
     minikube_init
-    build_images
     config_load_balancer
+    build_images
     toogle_k8s_object "nginx" "create"
-    # toogle_k8s_object "ftps" "create"
-    # toogle_k8s_object "wordpress" "create"
-    # toogle_k8s_object "mysql" "create"
-    # toogle_k8s_object "phpMyAdmin" "create"
+    toogle_k8s_object "ftps" "create"
+    toogle_k8s_object "wordpress" "create"
+    toogle_k8s_object "mysql" "create"
+    toogle_k8s_object "phpMyAdmin" "create"
     toogle_k8s_object "grafana" "create"
     toogle_k8s_object "influxdb" "create"
 }
