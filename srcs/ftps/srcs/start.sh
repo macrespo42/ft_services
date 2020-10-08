73c4809260dfbef31c183774c004a8c0286deebe
macrespo42 <maxime.crespopro@gmail.com>
2020-10-08 15:33:53 +0200

    fix ftps + final version

--- srcs/ftps/srcs/start.sh
+++ srcs/ftps/srcs/start.sh
@@ -1,23 +1,14 @@
 #!/bin/sh
 
-if [ -z "$FTPS_USER" ]; then
-    FTPS_USER="admin"
-fi
+NAME=admin
+PASS=admin
+FOLDER="/ftp/user"
 
-if [ -z "$FTPS_PASSWORD" ]; then
-    FTPS_PASSWORD="1234"
-fi
+echo -e "$PASS\n$PASS" | adduser -h $FOLDER -s /sbin/nologin -u 1000 $NAME
 
-openssl req -x509 -nodes -days 7300 -newkey rsa:2048 -subj "/C=FR/ST=fr/L=Paris/O=42/CN=macrespo" -keyout /etc/ssl/certs/vsftpd.pem -out /etc/ssl/certs/vsftpd.pem
+mkdir -p $FOLDER
+chown $NAME:$NAME $FOLDER
+unset NAME PASS FOLDER
 
-mkdir -p /ftps/data/
-chmod a-w ftps/data
-
-addgroup ftps_users
-adduser -D --ingroup ftps_users $FTPS_USER
-echo "$FTPS_USER:$FTPS_PASSWORD" | chpasswd
-
-mkdir /ftps/data/$FTPS_USER
-chown -R $FTPS_USER:ftps_users /ftps/data/$FTPS_USER
-
-/usr/sbin/vsftpd /etc/vsftpd.conf
+telegraf &
+./usr/sbin/vsftpd -opasv_address=172.17.0.8 /etc/vsftpd/vsftpd.conf
